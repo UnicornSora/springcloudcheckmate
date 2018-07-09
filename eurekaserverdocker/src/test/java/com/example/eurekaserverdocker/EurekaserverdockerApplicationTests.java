9753202c45f4fd1d217f330b75cb467075a06c0b
zhangqiang <zhangqiang432@tasly.com>
2018-07-09 11:17:07 +0800

    spring cloud 断路器监控(Hystrix Dashboard)

---
+++ eurekaserverdocker/src/test/java/com/example/eurekaserverdocker/EurekaserverdockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.eurekaserverdocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaserverdockerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
